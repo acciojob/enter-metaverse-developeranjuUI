69f6a6217be3feed9528102f1d1c155848c8ad35
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-29 18:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const status = document.getElementById('status');
 const enterBtn = document.getElementById('enterBtn');
 
 enterBtn.addEventListener('click',()=>{
-	let newStatus = document.createElement('h1');
-	newStatus.id = 'status';
-	status.textContent = 'Entered Metaverse';
+	// let newStatus = document.createElement('h1');
+	// newStatus.id = 'status';
+	// newStatus.textContent = 'Entered Metaverse';
+	// status.replaceWith(newStatus);
+	status.innerHTML = '<h1>Entered Metaverse</h1>';
 })
