3ad179347dfebcd090c458d3882488b358e2694a
developeranjuUI <116105555+developeranjuUI@users.noreply.github.com>
2025-03-29 17:40:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const status = document.getElementById('status');
+const enterBtn = document.getElementById('enterBtn');
+
+enterBtn.addEventListener('click',()=>{
+	let newStatus = document.createElement('h1');
+	newStatus.textContent = 'Entered Metaverse';
+	status.replaceWith(newStatus);
+})
